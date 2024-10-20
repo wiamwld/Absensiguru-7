369998dbf4f7a723aa06e311b90c1360e2335b38
wiam <wiamwulandari10@gmail.com>
2024-10-20 13:04:25 +0700

    memperbaiki controller

--- app/Http/Controllers/Api/GuruController.php
+++ app/Http/Controllers/Api/GuruController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
-//import model Post
 use App\Models\Guru;
-
 use App\Http\Controllers\Controller;
-
-//import resource PostResource
 use App\Http\Resources\GuruResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GuruController extends Controller
-{    
+{
     /**
      * index
      *
@@ -22,7 +20,102 @@ class GuruController extends Controller
         //get all gurus
         $gurus = Guru::latest()->paginate(5);
 
-        //return collection of posts as a resource
+        //return collection of gurus as a resource
         return new GuruResource(true, 'List Data Gurus', $gurus);
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required|string|max:255',
+            'nip' => 'required|string|unique:gurus|max:20',
+            'jenis_kelamin' => 'required|in:laki-laki,perempuan',
+            'alamat' => 'required|string',
+            'no_hp' => 'required|string|max:15',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create guru
+        $guru = Guru::create($request->all());
+
+        //return response
+        return new GuruResource(true, 'Data Guru Berhasil Ditambahkan!', $guru);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function show($id)
+    {
+        //get guru by id
+        $guru = Guru::find($id);
+
+        //return response
+        return new GuruResource(true, 'Data Guru', $guru);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        //get guru by id
+        $guru = Guru::find($id);
+
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama' => 'sometimes|required|string|max:255',
+            'nip' => 'sometimes|required|string|max:20|unique:gurus,nip,' . $guru->id,
+            'jenis_kelamin' => 'sometimes|required|in:laki-laki,perempuan',
+            'alamat' => 'sometimes|required|string',
+            'no_hp' => 'sometimes|required|string|max:15',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //update guru
+        $guru->update($request->all());
+
+        //return response
+        return new GuruResource(true, 'Data Guru Berhasil Diupdate!', $guru);
+    }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function destroy($id)
+    {
+        //get guru by id
+        $guru = Guru::find($id);
+
+        //delete guru
+        $guru->delete();
+
+        //return response
+        return new GuruResource(true, 'Data Guru Berhasil Dihapus!');
     }
 }
