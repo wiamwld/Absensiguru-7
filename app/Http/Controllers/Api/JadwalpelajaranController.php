61ea7aa6bff4836266c187b54b20824ec6269ed1
wiam <wiamwulandari10@gmail.com>
2024-12-08 12:47:28 +0700

    menambahkan controler

--- app/Http/Controllers/Api/JadwalpelajaranController.php
+++ app/Http/Controllers/Api/JadwalpelajaranController.php
@@ -1,11 +1,63 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Jadwalpelajaran;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
 
 class JadwalpelajaranController extends Controller
-{
-    //
+{    
+    
+    public function index()
+    {
+        $Jadwalpelajaran = Jadwalpelajaran::all();
+
+        return response()->json($Jadwalpelajaran);
+    }
+     public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'mapel'     => 'required',
+            'nama_guru'     => 'required',
+            'ruang_kelas'   => 'required',
+            'waktu_mulai'   => 'required',
+            'waktu_selesai'   => 'required',
+            'tanggal'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create post
+        $jadwalpelajaran = Jadwalpelajaran::create([
+            'mapel'     => $request->mapel,
+            'nama_guru'     => $request->nama_guru,
+            'ruang_kelas'   => $request->ruang_kelas,
+            'waktu_mulai'   => $request->waktu_mulai,
+            'waktu_selesai'   => $request->waktu_selesai,
+            'tanggal'   => $request->tanggal,
+        ]);
+
+        //return response
+        return response()->json([
+            'success'=>true,
+            'mesage'=>'data berhasil ditambahkan',
+            'data'  => $jadwalpelajaran
+        ]);
+    }
+    public function show($id)
+    {
+        $jadwalpelajaran = Jadwalpelajaran::find($id);
+
+        return response()->json([
+            'success'=>true,
+            'mesage'=>'data berhasil ditampilkan',
+            'data'  => $jadwalpelajaran
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GuruController;
+use App\Http\Controllers\Api\AbsensiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,5 @@ Route::get('guru/{id}',[GuruController::class,'show']);
 Route::get('absensi', [AbsensiController::class, 'index']);
 Route::post('absensi', [AbsensiController::class, 'store']);
 Route::get('absensi/{id}',[AbsensiController::class,'show']);
+Route::put('absensi/{id}', [AbsensiController::class, 'update']);
+Route::delete('absensi/{id}', [AbsensiController::class, 'destroy']);
